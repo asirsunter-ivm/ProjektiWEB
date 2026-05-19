6607832c5c00b1a60ab8f640a902d9bff7769381
asirsunter-ivm <asir.sunter@fshnstudent.info>
2026-05-19 23:43:46 +0200

    Auth module: added code to all files

---
+++ includes/config.php
@@ -0,0 +1,26 @@
+<?php
+// Database
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'postaweb');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+ 
+// Encryption key (32 karaktere - mos e ndrysho pas instalimit!)
+define('ENCRYPTION_KEY', 'PostaWeb_AES_Key_2026_FiniteLoop!');
+define('ENCRYPTION_CIPHER', 'AES-256-CBC');
+ 
+// PayPal Sandbox
+define('PAYPAL_CLIENT_ID', 'VENDOS_KETU_PAYPAL_CLIENT_ID');
+define('PAYPAL_SECRET',    'VENDOS_KETU_PAYPAL_SECRET');
+define('PAYPAL_MODE',      'sandbox'); // ndrysho ne 'live' per prodhim
+ 
+// SMTP (Gmail)
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USER', 'postaweb.finiteloop@gmail.com');
+define('SMTP_PASS', 'VENDOS_KETU_APP_PASSWORD');
+define('SMTP_PORT', 587);
+ 
+// Site
+define('SITE_URL', 'http://localhost/postaweb');
+define('SITE_NAME', 'PostaWeb');
+ ?>
